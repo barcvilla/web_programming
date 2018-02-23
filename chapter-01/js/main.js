bec37dbe105ab3c593531232c39c698093cd1751
Carlos Villanueva <ceva_19@hotmail.com>
2018-02-23 00:43:23 -0300

    Second JavaScript Program

--- chapter-01/js/main.js
+++ chapter-01/js/main.js
@@ -1,4 +1,7 @@
-const btn = document.getElementById('button1');
+window.onload = change;
+
+const btn = window.document.getElementById('button1');
+
 const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'rebeccapurple', 'violet'];
 
 function change()
@@ -6,4 +9,12 @@ function change()
     document.body.style.background = rainbow[Math.floor(7 * Math.random)];
 }
 
-    btn.addEventListener('click', change, false);
+if(btn)
+{
+   btn.addEventListener('click', change, false);
+}
+else
+{
+    console.log("error...");
+}
+    
