4f31c8b9e00542b9c66630c423f68307f97d6010
Carlos Villanueva <barcvilla@gmail.com>
2018-04-23 22:47:30 -0300

    JavaScript Programming Basics

--- JavaScript-Ninja/js/ch02-01.js
+++ JavaScript-Ninja/js/ch02-01.js
@@ -16,12 +16,44 @@ function declarandoVariables()
     // const : se utiliza cuando la variable no se le asignara otro valor
     // let: se utiliza si la variable puede reasignarse mas adelante en el programa.
     const name = "Alexa"; // no se le asignara otra cadena a esta variable inmutable
-    let score = 0; // este valor puede cambiar a lo largo del programa
+    let length = 0; // este valor puede cambiar a lo largo del programa
+    length = name.length;
+    console.log(length);
+    console.log(name.toUpperCase());
+    console.log(name.toLowerCase());
+    /* obtenemos el elemento de una posicion indicada */
+    console.log(name.charAt(2));
+    /* obtenemos saber cual es el indice de un elemento de la cadena */
+    console.log(name.indexOf('x'));
+    /* obtenemos la ultima aparicion de un caracter */
+    console.log(name.lastIndexOf('a'));
+    /* queremos saber si una cadena contiene cierto caracter */
+    console.log(name.includes('t'));
+    /* verificamos si una cadena comienza con un cierto caracter */
+    console.log(name.startsWith('A'));
+    /* verificamos si una cadena termina con un cierto caracter */
+    console.log(name.endsWith('g'));
+    /* concatenamos dos cadenas */
+    console.log(name.concat(' y John'));
+    /* otra forma de concatenar cadenas */
+    console.log(name + ' y ' + 'John');
+    /* repetir la cadena n veces */
+    console.log(name.repeat(3));
+}
+
+function usingTemplateLiterals()
+{
+    const name = `Siri`;
+    console.log(`Hello ${name}`);
+
+    const age = 39;
+    console.log(`I will be ${age + 1} next year`);
 }
 
 function main()
 {
-    mensaje();
+    declarandoVariables();
+    usingTemplateLiterals();
 }
 
 main();
