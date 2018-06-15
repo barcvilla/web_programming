e78ef0c6f2d545e0f1f065610c0471a97ca82cd0
Carlos Villanueva <ceva_19@hotmail.com>
2018-06-14 22:55:11 -0300

    JavaScript ch03 - StructureData

--- JavaScript-Ninja/Quiz-Ninja/main.js
+++ JavaScript-Ninja/Quiz-Ninja/main.js
@@ -1,3 +1,23 @@
+/*
 const question = "What is Superman's real name?";
-const answer = prompt(question); /*prompt nos permite solicitar al usuario un ingreso de dato*/
+const answer = prompt(question); 
 alert(`You answered ${answer}`);
+/*prompt nos permite solicitar al usuario un ingreso de dato*/
+
+const quiz = [["What is Superman's real name?","Clark Kent"], ["What is Wonder Woman's real name", "Diana Prince"], 
+              ["What is Batman's real name", "Bruce Wayne"]];
+let score = 0;
+
+for(const [question, answer] of quiz)
+{
+    const response = prompt(question);
+    if(response == answer)
+    {
+        alert('Correct!!!');
+        score++;
+    }
+    else
+    {
+        alert(`Wrong! The correct answer was ${answer}`);
+    }
+}
--- JavaScript-Ninja/js/ch03-06.js
+++ JavaScript-Ninja/js/ch03-06.js
@@ -41,12 +41,87 @@ function forLoopII()
     }
 }
 
+/**Loop aninados: Colocamos un loop dentro de otro Loop */
+function nestedLoop()
+{
+    let j = 0;
+    for(let i = 1; i < 13; i++)
+    {
+        j = 1;
+        for(; j < 13; j++ )
+        {
+            console.log(`${i} multiplicado por ${j} es ${i * j}`);
+        }
+    }
+}
+
+/**Looping sobre Array */
+function loopOverArray()
+{
+    let avengers = ['Captian America', 'Iron Man', 'Thor', 'Hulk'];
+    for(let i = 0, max = avengers.length; i < max; i++)
+    {
+        console.log(avengers[i]);
+    }
+}
+
+/**For of looping */
+function forOfLoop()
+{
+    let heroes = ['Captian America', 'Iron Man', 'Thor', 'Hulk'];
+    for(const value of heroes)
+    {
+        console.log(value);
+    }
+}
+
+/**Looping sobre Set: Set es enumerable, lo que significa que tiene metodos que nos permiten iterar sobre cada valor en el set.
+ * La iteracion es en el mismo orden que ellos fueron adicionados en el set.
+*/
+function loopOverSets()
+{
+    let letters = new Set('hello');
+    for(const letter of letters)
+    {
+        console.log(letter);
+    }
+}
+
+/**Looping over Maps */
+function loopOverMaps()
+{
+    let romanNumerals = new Map();
+    romanNumerals.set(1, 'I',).set(2, 'II').set(3, 'III').set(4, 'IV').set(5, 'V');
+    for(const key of romanNumerals.keys())
+    {
+        console.log(key);
+    }
+
+    /**el metodo value() nos permite iterar sobre los valores */
+    for(const value of romanNumerals.values())
+    {
+        console.log(value);
+    }
+
+    /**Si queremos acceder a la clave/valor podemos usar el metodo entries() */
+    for(const [key, value] of romanNumerals.entries())
+    {
+        console.log(`${key} in Roman numerals is ${value}`);
+    }
+}
+
 function main()
 {
     whileLoop();
     doWhileLoop();
     forLoopI();
     forLoopII();
+    nestedLoop();
+    loopOverArray();
+    forOfLoop();
+    loopOverSets();
+    loopOverMaps();
+
 }
 
 main();
